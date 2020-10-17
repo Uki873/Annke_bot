3f5a1d64f5e38fead96552f90812ecdb0794b4f7
yNoth <69696615+Uki873@users.noreply.github.com>
2020-10-17 14:55:39 -0300

    Create slowmode.js

---
+++ slowmode.js
@@ -0,0 +1,16 @@
+\\\
+Isso server para deixar seu chat em modo lento.
+\\\
+
+const Discord = require('discord.js')
+
+exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`Você não tem permissão para usar este comando`);
+    message.channel.setRateLimitPerUser(args[0])
+    message.reply(`O tempo do Slowmode alterado com sucesso para ${args[0]} segundos!`)
+}
+
+exports.help = {
+    name: 'slowmode',
+    aliases: ['slow']
+}
