d38de6ffa66fcbed3caf4cd4e9577bca4bc2c5fb
yNoth <69696615+Uki873@users.noreply.github.com>
2020-11-25 10:27:24 -0300

    Update and rename help (1).txt to help (1).js

---
+++ help (1).js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const config = require('../config.json')
+
+exports.run = async(bot, message, args) => { 
+
+    let avatar = message.author.displayAvatarURL({format: 'png'});
+const embed = new Discord.MessageEmbed()
+   .setAuthor('Alone™ -2.0.0A', 'https://images-ext-2.discordapp.net/external/AF4HkhT5ipvq7wNll6GkfRqbt6HY0Q4nrjxO9mjDRF8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/778636554386145331/adb03a20a79a238980dd396c4218ed12.webp?width=395&height=395')
+   .setColor('#63a7ff')
+    .setDescription(`Olá **${message.author.username}**, meu prefixo neste servidor **\`\`${message.guild.name}\`\`** é: **${config.prefix}** . Este é minha **central de comandos**, abaixo você poderá ver todos meus comandos, **separados por categorias**.`)
+   
+    .setThumbnail(`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=2048`)
+    .addField("**:hammer:➤  Comandos Moderação**", "**``/warn`` ``/listban`` ``/caifora`` ``/ban`` ``/kick`` ``/mute`` ``/unmute``**")
+    .addField("**:dizzy:➤  Comandos Úteis**", "**``/anuncio`` ``/enquete`` ``/sorteio`` ``/lock`` ``/unlock`` ``/modo-lento`` ``/criar_canal`` ``/clear`` ``info`` ** ``ping`` **``uptime``**")
+
+    .addField("**:rocket:➤  Comandos Entrentenimento**", "**``/stronks`` ``/biscoito`` ``/ship`` ``/laranjo`` ``/kiss`` ``/avatar`` ``/wanted`` ``/slap``**")
+    .setTimestamp()
+    .setFooter(message.author.tag, avatar);
+
+    message.channel.send(embed);
+}
