bfb9d9ed8c7622d82af75ec214018444f4c83e6c
yNoth <69696615+Uki873@users.noreply.github.com>
2020-11-26 16:44:15 -0300

    Update help.js

--- help.js
+++ help.js
@@ -5,7 +5,7 @@ exports.run = async(bot, message, args) => {
 
     let avatar = message.author.displayAvatarURL({format: 'png'});
 const embed = new Discord.MessageEmbed()
-   .setAuthor('Alone™ -2.0.0A', 'https://images-ext-2.discordapp.net/external/AF4HkhT5ipvq7wNll6GkfRqbt6HY0Q4nrjxO9mjDRF8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/778636554386145331/adb03a20a79a238980dd396c4218ed12.webp?width=395&height=395')
+   .setAuthor('Annke™ -2.0.0A', 'https://images-ext-2.discordapp.net/external/AF4HkhT5ipvq7wNll6GkfRqbt6HY0Q4nrjxO9mjDRF8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/778636554386145331/adb03a20a79a238980dd396c4218ed12.webp?width=395&height=395')
    .setColor('#63a7ff')
     .setDescription(`Olá **${message.author.username}**, meu prefixo neste servidor **\`\`${message.guild.name}\`\`** é: **${config.prefix}** . Este é minha **central de comandos**, abaixo você poderá ver todos meus comandos, **separados por categorias**.`)
    
